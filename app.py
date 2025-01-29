82436f373b4a1f88ed7fd883e1063731c1eea2ed
Ali Khorsandfard <secure73@gmail.com>
2025-01-29 21:43:34 +0100

    run

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from model.User import User
+from User import User
 #from model.User import User
 myUser = User()
 #myUser.create("donald@trump.com","i am the President of Unted State","President 45-47 Donald Trump")
